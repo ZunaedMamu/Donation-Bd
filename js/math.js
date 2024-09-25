c0373f3577b7113498ddd2f98ca3c8d32026eccb
ZunaedMamu <zunaied19988@gmail.com>
2024-09-26 05:16:13 +0600

    Done

--- js/math.js
+++ js/math.js
@@ -7,7 +7,7 @@ document.getElementById("btn-donate-now-newkhali").addEventListener("click",func
    const currentAmount = getTextFieldById("newkhali-current-amount");
    const donatedAmount = getInputFieldById("newkhali-donation");
    
-  if(myCurrentAmount>donatedAmount && myCurrentAmount>0){
+    if(myCurrentAmount >= donatedAmount && donatedAmount >0){
     const newDonatedAmount = currentAmount + donatedAmount;
     const myAmount = myCurrentAmount - donatedAmount;
 
@@ -29,6 +29,7 @@ document.getElementById("btn-donate-now-newkhali").addEventListener("click",func
  
     console.log(currentAmount,donatedAmount, newDonatedAmount, myAmount);
   }
+  
   else{
     alert("Invalid Amount");
   }
@@ -43,7 +44,7 @@ document.getElementById("btn-donate-now-feni").addEventListener("click",function
    const currentAmount2 = getTextFieldById("feni-current-amount");
    const donatedAmount2 = getInputFieldById("feni-donation");
    
-  if(myCurrentAmount>donatedAmount2 && myCurrentAmount > 0){
+    if(myCurrentAmount >= donatedAmount2 && donatedAmount2 > 0){
     const newDonatedAmount2 = currentAmount2 + donatedAmount2;
     const myAmount2 = myCurrentAmount - donatedAmount2;
 
@@ -63,6 +64,7 @@ document.getElementById("btn-donate-now-feni").addEventListener("click",function
  
     console.log(currentAmount2,donatedAmount2, newDonatedAmount2, myAmount2);
   }
+ 
   else{
     alert("Invalid Amount");
   }
@@ -75,9 +77,11 @@ document.getElementById("btn-donate-now-quota").addEventListener("click",functio
     const currentAmount3 = getTextFieldById("quota-current-amount");
     const donatedAmount3 = getInputFieldById("quota-donation");
     
-   if(myCurrentAmount > donatedAmount3 && myCurrentAmount > 0){
+     if(myCurrentAmount >= donatedAmount3 && donatedAmount3 > 0 ){
      const newDonatedAmount3 = currentAmount3 + donatedAmount3;
      const myAmount3 = myCurrentAmount - donatedAmount3;
+
+     card.classList.remove('hidden');
 
      document.getElementById("current-amount").innerText= myAmount3;
      document.getElementById("quota-current-amount").innerText=newDonatedAmount3;
@@ -93,6 +97,7 @@ document.getElementById("btn-donate-now-quota").addEventListener("click",functio
   
      console.log(currentAmount3,donatedAmount3, newDonatedAmount3, myAmount3);
    }
+ 
    else{
      alert("Invalid Amount");
    }
